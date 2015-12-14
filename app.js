44f9323597554e22d960b0edcb688bcba1b344b1
Yannis Guedel <me@yannisguedel.ch>
2015-12-14 19:19:20 +0100

    fixed port for azure

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use("/api/peopleSearch", require("./src/api/peopleSearchApi"))
 
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
